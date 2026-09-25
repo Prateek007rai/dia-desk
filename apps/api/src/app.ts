c0a48d7416abe8d3887cea04fb60c5f4bd201bc5
Prateek Rai <Prateekrai341036@gmail.com>
2026-09-25 16:51:14 +0530

    Add express middlewares

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,17 @@
 import express, { Application, Request, Response } from "express";
 
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 const app: Application = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 
 // Basic route for testing
 app.get("/health", (req: Request, res: Response) => {
